ad2dfc69c2cfb380de8552f4f6bc8d5e3393a4b6
AlanGarciaM <57108764+AlanGaMa85@users.noreply.github.com>
2019-11-25 18:54:33 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
+<!DOCTYPE html>
+
 <html>
-<head>
-<title>My Page</title>
-</head>
-<body>
-<form name="myform" action="http://www.mydomain.com/myformhandler.cgi" method="POST">
-<div align="center"><br>
-<input type="checkbox" name="option1" value="Milk"> Milk<br>
-<input type="checkbox" name="option2" value="Butter" checked> Butter<br>
-<input type="checkbox" name="option3" value="Cheese"> Cheese<br>
-<br>
-</div>
-</form>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Surus1</title>
+    </head>
+    <body>
+        <?php
+          echo "esto es php"
+        ?>
+    </body>
 </html>
